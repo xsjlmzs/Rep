b920ce08a6db2fc573975f51a0605db1870e51fb
xsjlmzs <1498020741@qq.com>
2023-04-19 11:06:27 +0800

    debuging

--- server/server.cc
+++ server/server.cc
@@ -252,7 +252,7 @@ namespace taas
         // send batch_subtxns to all in-region peers
         for (std::map<uint32, PB::MessageProto>::iterator iter = batch_subtxns.begin(); iter != batch_subtxns.end(); ++iter)
         {
-            iter->second.set_debug_info("Distribute" + std::to_string(epoch));
+            iter->second.set_debug_info(std::to_string(epoch));
             conn_->Send(iter->second);
         }
         LOG(INFO) << "epoch : " << epoch << " have sent " << batch_subtxns.size() << " Distribute() msgs and barrier";
@@ -421,7 +421,7 @@ namespace taas
         for (std::map<uint32, PB::MessageProto>::iterator iter = batch_replies.begin();
             iter != batch_replies.end(); ++iter)
         {
-            iter->second.set_debug_info("Merge" + std::to_string(epoch));
+            iter->second.set_debug_info(std::to_string(epoch));
             conn_->Send(iter->second);
             sent_cnt++;
         }
